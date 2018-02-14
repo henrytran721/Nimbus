3cd65ceffbafb75c7869386d2aa312a8d18bcd64
Chester Vanhuang <cvanhuan@ucsd.edu>
2018-02-14 12:30:00 -0800

    fix onclick

--- public/js/reflect.js
+++ public/js/reflect.js
@@ -52,7 +52,7 @@ function addList() {
     li.appendChild(span);
     
     for( i = 0; i < close.length; i++ ) {
-        close[i].onClick = function() {
+        close[i].onclick = function() {
             var div = this.parentElement;
             div.style.display = "none";
         }
