01af4da7e79100a9e34c3d126b429e48ecb7d36f
Chester Vanhuang <cvanhuan@ucsd.edu>
2018-02-14 23:12:55 -0800

    progress json data

--- routes/progress.js
+++ routes/progress.js
@@ -1,6 +1,8 @@
+var data = require("../progress_data.json");
 
 exports.viewProgress = function(req, res) {
     res.render('progress', {
-
+        "topic": data['topic'],
+        "progress_data":data['progress']
     });
 }
