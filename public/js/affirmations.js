c95aa80c06ff6b8ef700d514a75f3bb838c42b25
Chester Vanhuang <cvanhuan@ucsd.edu>
2018-02-15 19:52:03 -0800

    fix random affirmations

--- public/js/affirmations.js
+++ public/js/affirmations.js
@@ -1,12 +1,29 @@
+'use strict';
 
-		var affirmations = ['Strive For Greatness', 'I feel good today', 'affirmation 3']
-		console.log(affirmations['0'])
-		document.getElementById("affirmations").innerHTML = affirmations[0];
+$(document).ready(function() {
+    initializePage();
+})
+
+function initializePage() {
+	document.getElementById("affirmations").innerHTML = "I believe in you";
+}
+
+	//console.log(affirmations['0']);
+		//document.getElementById("affirmations").innerHTML = affirmations[0];
+
+	function getRandom() {
+		var affirmations = ['Strive For Greatness', 'I feel good today', 'I got this'];
+		var randomNumber = Math.floor(Math.random()*affirmations.length);	
+		//console.log(randomNumber);
+		var randomString = affirmations[randomNumber];
+		//console.log(randomString);
+		document.getElementById("affirmations").innerHTML = randomString;
+	}
 
 
-		function randomString() {
+/*		function randomString() {
 			$("p").click(function(){
-        alert("The affirmation is clicked.");
+        	alert("The affirmation is clicked.");
     	});
 
 		var randomNumber = Math.floor(Math.random()*textArray.length);
@@ -14,3 +31,4 @@
 		console.log(randomString)
 
 		}
+*/
